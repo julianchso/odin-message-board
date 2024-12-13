97e0e352208eda8df32e11df9928decaa9b435d0
Julian So <ch.julian.so@gmail.com>
2024-12-13 18:31:10 -0500

    added message to show on frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import './App.css';
 import axios from 'axios';
 // import { PORT } from '../../.env';
 
-import { messages } from './messages';
-
 // const PORT = process.env.PORT;
 
 function App() {
@@ -14,9 +12,8 @@ function App() {
     // TODO: make port 3000 a variable from process.env
     try {
       const response = await axios.get('http://localhost:3000/messages');
-      console.log(typeof response.data);
-      setMessages(response.data);
-      console.log(response.data);
+      setMessages(response.data.messages);
+      console.log(response.data.messages);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -29,23 +26,18 @@ function App() {
   return (
     <>
       <h1>Mini Messaging App</h1>
-      <h2>{Object.values(messages)}</h2>
       <div className='messagesCtn'>
         <div>
-          {typeof messages === 'undefined' ? (
-            <p>Loading...</p>
+          {messages === '' ? (
+            <p>loading...</p>
           ) : (
-            // Object.values(messages).map((message) => {
-            messages.map((message) => {
-              return (
-                <>
-                  <p>{message.user}</p>
-                  <p>{message.text}</p>
-                  <p>{message.added}</p>
-                  <p>{message.id}</p>
-                </>
-              );
-            })
+            messages.map((message) => (
+              <div key={message.id}>
+                <p>{message.user}</p>
+                <p>{message.text}</p>
+                <p>{message.added}</p>
+              </div>
+            ))
           )}
         </div>
       </div>
